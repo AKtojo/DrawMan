c0e3e866a791c4c27212573e098b1e26f3307c14
Simone Ippoliti <simoneippoliti53@gmail.com>
2021-05-23 19:39:19 +0200

    Fix glitchy character movement

--- Assets/DrawMan/Scripts/PlayerController.cs
+++ Assets/DrawMan/Scripts/PlayerController.cs
@@ -1,92 +1,53 @@
 using UnityEngine;
+using DrawMan.Components;
 using DrawMan.Core.ActionSystem;
-using DrawMan.Core.DataContainers;
 
 namespace DrawMan.Core
 {
     public class PlayerController : MonoBehaviour
     {
-        private Vector2 m_gravityDirection = Vector2.down;
-        private Vector2 m_verticalVelocity = Vector2.zero;
-
         [Header("Components references")]
-        [SerializeField] private DirectionData m_relativeDirection;
-        //[SerializeField] private CapsuleCollider2D m_collider;
         [SerializeField] private Rigidbody2D m_rigidbody;
-        [SerializeField] private Transform m_groundCheckTransform;
-        [SerializeField] private Transform m_ceilingCheckTransform;
+        [SerializeField] private GroundCheck m_groundCheck;
+        [SerializeField] private CeilingCheck m_ceilingCheck;
 
         [Header("Parameters")]
-        [SerializeField] LocomotionSettings m_locomotionSettings;
+        [SerializeField] [Min(0.0f)] private float m_maxSpeed;
+        [SerializeField] [Min(0.0f)] private float m_jumpImpulseForce;
+        [SerializeField] [Min(0.0f)] private float m_minSpeed;
         [Space]
         [Min(0.0f)]
-        [Tooltip("Instance's current gravity acceleration multiplier while falling")]
-        [SerializeField] private float m_gravityMultiplier;
+        [Tooltip("Gravity acceleration while falling (m/s)")]
+        [SerializeField] private float m_gravityAccel;
 
         [Min(0.0f)]
-        [Tooltip("Instance's current gravity acceleration multiplier while jumping")]
-        [SerializeField] private float m_jumpGravityMultiplier;
+        [Tooltip("Gravity acceleration while jumping (m/s)")]
+        [SerializeField] private float m_jumpGravityAccel;
 
         [Min(0.0f)]
-        [Tooltip("Maximum m/s this instance can travel")]
-        [SerializeField] private float m_maximumFallingSpeed;
+        [Tooltip("Maximum velocity the player can fall at (m/s)")]
+        [SerializeField] private float m_maxFallingSpeed;
 
-        [Header("\tGround check")]
-        [SerializeField] LayerMask m_groundCheckLayers;
-        [SerializeField] LayerMask m_ceilingCheckLayers;
-
-        [Space]
-        [Tooltip("Radius of the circle that checks for ground")]
-        [SerializeField] private float m_maxWalkableSlope;
-
-        [Tooltip("Radius of the circle that checks for ground")]
-        [SerializeField] private float m_circleRadius;
-
-        [Tooltip("Minimum distance to check for collision from instance")]
-        [SerializeField] private float m_checkDistance;
-
-        [Tooltip("Maximum depth to check for ground.\nMust be > 0")]
-        [SerializeField] private float m_maxDepthCheck;
-
-        [Tooltip("Minimum depth to check for ground.\nMust be < 0")]
-        [SerializeField] private float m_minDepthCheck;
-
-        [Min(1)]
-        [Tooltip("Maximum number of contact points to check per frame")]
-        [SerializeField] private int m_maxContacts;
-
-        [Header("Character Actions")]
+        [Header("Character Actions (Input)")]
         [SerializeField] private MovementAction m_movementAction;
         [SerializeField] private JumpAction m_jumpAction;
+
+        private Vector2 m_verticalVelocity = Vector2.zero;
+        private Vector2 velocity = Vector2.zero;
 
         private PlayerInput m_playerInput;
         private Transform m_transform;
 
-        private const float ACUTE_45_ANGLE = 45.0f;
         private const float RIGHT_ANGLE = 90.0f;
-        private const float STRAIGHT_ANGLE = 180.0f;
-        private const float OBTUSE_270_ANGLE = 270.0f;
-        private const float ROUND_ANGLE = 360.0f;
 
-        private float m_jumpInertia;
-
-        private bool m_grounded;
         private bool m_wasGrounded;
         private bool m_isFalling;
-        private bool m_ceilingBumped;
         private bool m_facingRight = true;
-
-        private RaycastHit2D m_groundHit;
-        private RaycastHit2D m_ceilingHit;
-
-        private ContactPoint2D[] m_contactPoints;
 
         private void Awake()
         {
             m_playerInput = new PlayerInput();
             m_transform = transform;
-
-            m_contactPoints = new ContactPoint2D[m_maxContacts];
         }
 
         private void OnEnable()
@@ -101,147 +62,72 @@ namespace DrawMan.Core
 
         private void FixedUpdate()
         {
-            //VectorBasedMovement();
-            m_isFalling = IsFalling();
+            // Init
+            m_groundCheck.CheckCollision();
 
-            CheckGround();
+            Vector2 up = m_transform.up;
+            Vector2 right = Vector2.Perpendicular(m_groundCheck.Down);
+            Vector2 dir = right * m_movementAction.MovementDirection.x;
+            Vector2 dirNorm = Vector3.Normalize(dir);
 
-            CheckCeiling();
+            Vector2 minVelocity = dirNorm * m_minSpeed;
 
-            ScaleGravity();
+            velocity = dir * m_maxSpeed;
 
-            CheckLanding();
+            m_isFalling = IsFalling(-up, m_verticalVelocity);
+            float gravityAccel = m_isFalling ? m_gravityAccel : m_jumpGravityAccel;
 
-            PerformJump();
+            // Apply gravity
+            ApplyGravity(gravityAccel);
 
-            //Vector2 movementDirection = canMove || !m_grounded ?
-            //    (Vector2)Vector3.Cross(Vector3.back, m_groundHit.normal) :
-            //    Vector2.zero;
-
-            Vector2 movementDirection = m_relativeDirection.Right;
-
-            float movementVal = m_movementAction.MovementDirection.x;
-
-            Vector2 horizontalVelocity = GetHorizontalVelocity(movementDirection, movementVal);
-
-            Flip(movementVal);
-
-            // apply movement;
-            m_rigidbody.velocity = m_verticalVelocity + horizontalVelocity;
-        }
-
-        private void CheckGround()
-        {
-            m_wasGrounded = m_grounded;
-
-            float distance = m_checkDistance + 0.2f;
-
-            m_groundHit = Physics2D.CircleCast(m_groundCheckTransform.position, m_circleRadius,
-                m_gravityDirection, distance, m_groundCheckLayers, m_minDepthCheck, m_maxDepthCheck);
-
-            if (m_groundHit.collider == null)
+            // Clamp velocity
+            if (velocity != Vector2.zero && velocity.sqrMagnitude < minVelocity.sqrMagnitude)
             {
-                m_grounded = false;
-                return;
+                velocity = minVelocity;
             }
 
-            float dist = Vector2.Distance(m_groundCheckTransform.position, m_groundHit.point);
-
-            float angle = Vector2.Angle(m_groundHit.normal, -m_gravityDirection);
-
-            m_grounded = dist < m_checkDistance && angle <= m_maxWalkableSlope;
-
-            if (m_grounded && !m_wasGrounded)
+            // Jump
+            if (m_jumpAction.Jump && m_groundCheck.Grounded)
             {
-                m_verticalVelocity = m_groundHit.normal * m_locomotionSettings.GravityForce * -m_gravityMultiplier;
+                // OnJump event
+                m_verticalVelocity = up * m_jumpImpulseForce;
+                m_groundCheck.Clear();
             }
-            else if (m_wasGrounded && !m_grounded && m_isFalling)
+
+            if (m_isFalling)
+                m_verticalVelocity = Vector2.ClampMagnitude(m_verticalVelocity, m_maxFallingSpeed);
+
+            // Apply velocity
+            m_rigidbody.velocity = velocity + m_verticalVelocity;
+
+            m_wasGrounded = m_groundCheck.Grounded;
+            m_ceilingCheck.Clear();
+        }
+
+        private void ApplyGravity(float gravityAccel)
+        {
+            if (m_wasGrounded && !m_groundCheck.Grounded)
             {
-                m_verticalVelocity = Physics2D.gravity * m_gravityMultiplier;
+                m_verticalVelocity = m_groundCheck.Down * m_gravityAccel;
             }
-        }
-
-        private void CheckCeiling()
-        {
-            m_ceilingBumped = false;
-            float angle = Mathf.Atan2(m_gravityDirection.y, m_gravityDirection.x);
-
-            m_ceilingHit =
-                Physics2D.BoxCast(m_ceilingCheckTransform.position, Vector2.one,
-                angle, -m_gravityDirection, m_checkDistance, m_ceilingCheckLayers,
-                m_minDepthCheck, m_maxDepthCheck);
-
-            m_ceilingBumped =
-                m_ceilingHit.collider != null &&
-                m_ceilingHit.distance < m_checkDistance &&
-                !m_isFalling &&
-                Vector2.Angle(m_gravityDirection, m_ceilingHit.normal) < RIGHT_ANGLE;
-        }
-
-        private void ScaleGravity()
-        {
-            float gravityScale = m_isFalling ? m_gravityMultiplier : m_jumpGravityMultiplier;
-
-            m_verticalVelocity = m_grounded ?
-                Vector2.zero :
-                (m_verticalVelocity + (Physics2D.gravity * Time.fixedDeltaTime * gravityScale));
-
-            // set the maximum falling speed
-            m_verticalVelocity = m_ceilingBumped ?
-                m_gravityDirection :
-                Vector2.ClampMagnitude(m_verticalVelocity, m_maximumFallingSpeed);
-        }
-
-        private void CheckLanding()
-        {
-            // just landed and is falling
-            if (!m_wasGrounded && m_grounded && m_isFalling)
+            else if (!m_wasGrounded && m_groundCheck.Grounded)
             {
-                m_jumpInertia = 0.0f;
-                // onLand event
-                //Debug.Log("Implement onLand event!");
+                // OnLand event
             }
-        }
-
-        private void PerformJump()
-        {
-            // check and perform jump
-            if (m_jumpAction.Jump && m_grounded)
+            else
             {
-                m_verticalVelocity = m_gravityDirection * -m_locomotionSettings.JumpForce;
-                m_jumpInertia = m_movementAction.MovementDirection.x * m_locomotionSettings.MaxAiredSpeed;
-                m_grounded = false;
-
-                // onJump event
-                //Debug.Log("Implement onJump event!");
+                m_verticalVelocity += (m_groundCheck.Down * gravityAccel) * Time.fixedDeltaTime;
             }
-        }
 
-        private Vector2 GetHorizontalVelocity(Vector2 direction, float movementValue)
-        {
-            // change movement speed based on grounded state
-            //float speed = m_grounded ?
-            //    m_locomotionSettings.MaxMovementSpeed :
-            //    m_locomotionSettings.MaxAiredSpeed;
-
-            //return direction * movementValue * speed;
-
-            Vector2 hDir = direction * movementValue;
-
-            return m_grounded ?
-                hDir * m_locomotionSettings.MaxMovementSpeed :
-                hDir * m_locomotionSettings.MaxAiredSpeed;
-        }
-
-        // it works, needs tuning and corrections
-        private Vector2 GetAiredMovement(Vector2 direction, float movementValue)
-        {
-            float inertia = m_jumpInertia;
-
-            float value = m_jumpInertia - m_locomotionSettings.JumpInertiaFalloff * m_locomotionSettings.MaxAiredSpeed * Time.fixedDeltaTime;
-            m_jumpInertia = Mathf.Clamp(value, 0.0f, m_jumpInertia);
-
-            return (direction * movementValue) + (direction * inertia);
+            if (m_groundCheck.Grounded && m_isFalling)
+            {
+                m_verticalVelocity = m_groundCheck.Down * m_gravityAccel;
+            }
+            else if (m_ceilingCheck.Contact)
+            {
+                m_verticalVelocity = Vector2.zero;
+                m_ceilingCheck.Clear();
+            }
         }
 
         private void Flip(float movementValue)
@@ -260,19 +146,16 @@ namespace DrawMan.Core
             }
         }
 
-        public void SetGravityDirection(Vector3 direction)
-        {
-            m_gravityDirection = m_relativeDirection.Down;
-            Physics2D.gravity = m_gravityDirection * m_locomotionSettings.GravityForce;
-        }
+        private bool IsFalling(Vector2 down, Vector2 direction) => Vector2.Angle(down, direction) <= RIGHT_ANGLE;
 
-        public void SetGravityDirection(float x, float y, float z)
+        private void OnGUI()
         {
-            m_gravityDirection = m_relativeDirection.Down;
-            Physics2D.gravity = m_gravityDirection * m_locomotionSettings.GravityForce;
+            GUI.TextArea(new Rect(0, 0, 255, 255),string.Format("X: {0}\nY: {1}\nVelocity: {2}\nVertical: {3}\nGrounded: {4}",
+                m_movementAction.MovementDirection.x,
+                m_movementAction.MovementDirection.y,
+                velocity,
+                m_verticalVelocity,
+                m_groundCheck.Grounded));
         }
-
-        //private bool IsFalling => Vector3.Angle(m_gravityDirection, m_characterController.velocity) <= RIGHT_ANGLE;
-        private bool IsFalling() => !m_grounded && Vector2.Angle(m_gravityDirection, m_rigidbody.velocity) <= RIGHT_ANGLE;
     }
 }
